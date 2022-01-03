9bc4b9811035705ef99dde9dc5c0d3ea9bca4d49
Jaber Al Siam <jaber.siam@northsouth.edu>
2022-01-04 02:23:11 +0600

    implemented camera streaming from webcam using openCV

--- static/main.js
+++
@@ -1,6 +0,0 @@
-window.onload = function (){
-    let button = document.getElementById('test-button');
-    button.addEventListener('click', function(){
-        console.log('I am clicked');
-    });
-};
--- surveillance/views.py
+++ surveillance/views.py
@@ -1,12 +1,44 @@
-from django.shortcuts import render
+import threading
+from django.views.decorators import gzip
+from django.http import StreamingHttpResponse
+
+import cv2
 
 # Create your views here.
+from django.shortcuts import render
 
 
+@gzip.gzip_page
 def home(request):
+    try:
+        cam = VideoCamera()
+        return StreamingHttpResponse(gen(cam), content_type='multipart/x-mixed-replace;boundary=frame')
+    except:
+        pass
 
-    context = {
-        "name": "Home"
-    }
+    return render(request, 'index.html')
 
-    return render(request, 'index.html', context)
+
+class VideoCamera(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+        (self.grabbed, self.frame) = self.video.read()
+        threading.Thread(target=self.update, args=()).start()
+
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        image = self.frame
+        _, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
+
+    def update(self):
+        while True:
+            (self.grabbed, self.frame) = self.video.read()
+
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n'
